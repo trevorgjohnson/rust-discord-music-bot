69382b7f0e534bbb2b3042270e1940325458476d
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-08-02 17:43:20 -0500

    Rework to basic discord music bot

---
+++ src/commands.rs
@@ -0,0 +1,223 @@
+use std::time::Duration;
+
+use serenity::{
+    framework::standard::{Args, CommandResult},
+    model::prelude::Message,
+    prelude::Context,
+};
+
+use crate::utils::{check_msg, format_duration};
+
+pub async fn join(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).unwrap();
+    let guild_id = guild.id;
+
+    let channel_id = guild
+        .voice_states
+        .get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            check_msg(msg.reply(ctx, "Not in a voice channel").await);
+
+            return Ok(());
+        }
+    };
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let _handler = manager.join(guild_id, connect_to).await;
+
+    check_msg(msg.reply(ctx, "Joining").await);
+
+    Ok(())
+}
+
+pub async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let search = args.raw().collect::<Vec<&str>>().join(" ");
+
+    if search.starts_with("http") {
+        check_msg(msg.channel_id.say(&ctx.http, "Cannot be a url").await);
+        return Ok(());
+    }
+
+    let guild = msg.guild(&ctx.cache).unwrap();
+    let guild_id = guild.id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let fetched_lock = manager.get(guild_id);
+
+    if fetched_lock.is_none() {
+        join(ctx, msg).await?
+    };
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        let source = match songbird::input::ytdl_search(&search).await {
+            Ok(source) => source,
+            Err(err) => {
+                println!("Error starting source: {:?}", err);
+
+                check_msg(msg.channel_id.say(&ctx.http, "Error sourcing url").await);
+
+                return Ok(());
+            }
+        };
+
+        let song_metadata = source.metadata.clone();
+        let song_title = song_metadata
+            .title
+            .clone()
+            .unwrap_or("idk lmfao".to_owned());
+        let song_duration = song_metadata.duration.unwrap_or(Duration::from_secs(0));
+
+        let song_duration_str = format_duration(song_duration);
+
+        handler.enqueue_source(source);
+
+        check_msg(
+            msg.channel_id
+                .say(
+                    &ctx.http,
+                    format!("Playing **{}** _({})_", song_title, song_duration_str),
+                )
+                .await,
+        );
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Not in a voice channel to play in")
+                .await,
+        );
+    }
+
+    Ok(())
+}
+
+pub async fn queue(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild_id = msg.guild(&ctx.cache).unwrap().id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let handler = handler_lock.lock().await;
+
+        let queue = handler.queue().current_queue();
+
+        let queue_str = queue
+            .iter()
+            .enumerate()
+            .map(|(idx, track_handle)| {
+                format!(
+                    "**{}: {}** _({})_\n",
+                    idx,
+                    track_handle
+                        .metadata()
+                        .title
+                        .clone()
+                        .unwrap_or("idk lmfao".to_owned()),
+                    format_duration(
+                        track_handle
+                            .metadata()
+                            .duration
+                            .unwrap_or(Duration::from_secs(0))
+                    )
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("");
+
+        check_msg(msg.channel_id.say(&ctx.http, queue_str).await);
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Queue is currently empty")
+                .await,
+        );
+    }
+
+    Ok(())
+}
+
+pub async fn skip(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild_id = msg.guild(&ctx.cache).unwrap().id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let handler = handler_lock.lock().await;
+
+        let mut skipped_song_title = String::from("");
+        if let Some(current_metadata) = handler.queue().current() {
+            skipped_song_title = current_metadata
+                .metadata()
+                .title
+                .clone()
+                .unwrap_or("".to_owned());
+        }
+
+        match handler.queue().skip() {
+            Ok(_) => check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("Skipped **{}**", skipped_song_title))
+                    .await,
+            ),
+            Err(_) => check_msg(
+                msg.channel_id
+                    .say(&ctx.http, "There are no songs to skip")
+                    .await,
+            ),
+        };
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Queue is currently empty")
+                .await,
+        );
+    }
+
+    Ok(())
+}
+
+pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).unwrap();
+    let guild_id = guild.id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+    let has_handler = manager.get(guild_id).is_some();
+
+    if has_handler {
+        if let Err(e) = manager.remove(guild_id).await {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("Failed: {:?}", e))
+                    .await,
+            );
+        }
+
+        check_msg(msg.channel_id.say(&ctx.http, "Left voice channel").await);
+    } else {
+        check_msg(msg.reply(ctx, "Not in a voice channel").await);
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,95 @@
-mod weather;
+mod commands;
+mod utils;
 
-use anyhow::anyhow;
+use dotenv::dotenv;
 
-use serenity::http::CacheHttp;
-use serenity::model::application::interaction::{Interaction, InteractionResponseType};
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::model::prelude::command::CommandOptionType;
-use serenity::prelude::*;
-use serenity::{async_trait, model::prelude::GuildId};
+use serenity::{
+    async_trait,
+    client::{Client, Context, EventHandler},
+    framework::{
+        standard::{
+            macros::{command, group},
+            Args, CommandResult,
+        },
+        StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready},
+    prelude::GatewayIntents,
+};
+use songbird::SerenityInit;
 
-use shuttle_secrets::SecretStore;
-use tracing::{error, info};
-
-struct Bot {
-    weather_api_key: String,
-    client: reqwest::Client,
-    discord_guild_id: GuildId,
-}
+struct Handler;
 
 #[async_trait]
-impl EventHandler for Bot {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-
-        let commands =
-            GuildId::set_application_commands(&self.discord_guild_id, &ctx.http, |commands| {
-                commands
-                    .create_application_command(|command| {
-                        command.name("hello").description("Say Hello")
-                    })
-                    .create_application_command(|command| {
-                        command
-                            .name("weather")
-                            .description("Display the weather")
-                            .create_option(|option| {
-                                option
-                                    .name("place")
-                                    .description("City for lookup forecast")
-                                    .kind(CommandOptionType::String)
-                                    .required(true)
-                            })
-                    })
-            })
-            .await
-            .unwrap();
-
-        info!("{:#?}", commands);
-    }
-
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            let response_content = match command.data.name.as_str() {
-                "hello" => "hello".to_owned(),
-                "weather" => {
-                    let argument = command
-                        .data
-                        .options
-                        .iter()
-                        .find(|opt| opt.name == "place")
-                        .cloned();
-
-                    let value = argument.unwrap().value.unwrap();
-                    let place = value.as_str().unwrap();
-                    let result =
-                        weather::get_forecast(place, &self.weather_api_key, &self.client).await;
-
-                    match result {
-                        Ok(forecast) => {
-                            format!(
-                                "It is {}°C/{}°F in {}, {}",
-                                forecast.current.temp_c,
-                                forecast.current.temp_f,
-                                forecast.location.name,
-                                forecast.location.region
-                            )
-                        }
-                        Err(err) => {
-                            format!("Err: {}", err)
-                        }
-                    }
-                }
-                command => unreachable!("unknown command: {}", command),
-            };
-
-            let create_interaction_response =
-                command.create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(response_content))
-                });
-
-            if let Err(err) = create_interaction_response.await {
-                eprintln!("Cannot respond to slash command: {}", err)
-            }
-        }
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name)
     }
 }
 
-#[shuttle_runtime::main]
-async fn serenity(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = secret_store
-        .get("TOKEN")
-        .expect("'DISCORD_TOKEN' was not found");
-    let weather_api_key = secret_store
-        .get("WEATHER_API_KEY")
-        .expect("'WEATHER_API_KEY' was not found");
-    let guild_id = secret_store
-        .get("GUILD_ID")
-        .expect("'GUILD_ID' was not found");
+#[group]
+#[commands(join, play, queue, skip, leave)]
+struct General;
+
+#[command]
+#[only_in(guilds)]
+async fn join(ctx: &Context, msg: &Message) -> CommandResult {
+    crate::commands::join(ctx, msg).await
+}
+
+#[command]
+#[only_in(guilds)]
+async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    crate::commands::play(ctx, msg, args).await
+}
+
+#[command]
+#[only_in(guilds)]
+async fn queue(ctx: &Context, msg: &Message) -> CommandResult {
+    crate::commands::queue(ctx, msg).await
+}
+
+#[command]
+#[only_in(guilds)]
+async fn skip(ctx: &Context, msg: &Message) -> CommandResult {
+    crate::commands::skip(ctx, msg).await
+}
+
+#[command]
+#[only_in(guilds)]
+async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
+    crate::commands::leave(ctx, msg).await
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    // load all secrets in '.env' into the environment
+    dotenv().ok();
+    let token = std::env::var("TOKEN").expect("'TOKEN' was not found");
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("-"))
+        .group(&GENERAL_GROUP);
 
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot {
-            weather_api_key,
-            client: reqwest::Client::new(),
-            discord_guild_id: GuildId(guild_id.parse().unwrap()),
-        })
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .register_songbird()
         .await
         .expect("Err creating client");
 
-    Ok(client.into())
+    tokio::spawn(async move {
+        let _ = client
+            .start()
+            .await
+            .map_err(|err| println!("Client ended: {:?}", err));
+    });
+
+    let _ = tokio::signal::ctrl_c().await;
+    println!("Received Ctrl-C, shutting down.");
 }
